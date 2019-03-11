96a2e3c3170bd5d932c58a22451c0a5ccdca5f42
MichalHondowicz <40538831+MichalHondowicz@users.noreply.github.com>
2019-03-11 19:56:14 +0100

    First Routes added

--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -29,7 +29,7 @@ class Header extends Component {
                             </li>
                             <li className="nav-item">
                                 <a className="nav-link" href="login.html">
-                                    Login
+                                    Sign In
                                 </a>
                             </li>
                         </ul>
