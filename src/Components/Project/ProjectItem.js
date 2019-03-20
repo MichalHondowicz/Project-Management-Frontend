2864e26e24cdfc6331f0aa2327dd5ab4881ea1b8
MichalHondowicz <40538831+MichalHondowicz@users.noreply.github.com>
2019-03-20 20:03:55 +0100

    Actions added, code fixed in projectActions

--- src/Components/Project/ProjectItem.js
+++ src/Components/Project/ProjectItem.js
@@ -1,28 +1,28 @@
-import React, {Component} from 'react';
+import React, {Component} from "react";
 
-class projectItem extends Component {
+class ProjectItem extends Component {
     render() {
         return (
             <div className="container">
                 <div className="card card-body bg-light mb-3">
                     <div className="row">
                         <div className="col-2">
-                            <span className="mx-auto">REACT</span>
+                            <span className="mx-auto"></span>
                         </div>
                         <div className="col-lg-6 col-md-4 col-8">
                             <h3>Spring / React Project</h3>
-                            <p>Project to create a Project Manager with Spring Boot and React</p>
+                            <p>Project Management Application with Spring Boot and React</p>
                         </div>
                         <div className="col-md-4 d-none d-lg-block">
                             <ul className="list-group">
-                                <a href="#">
+                                <a href="# ">
                                     <li className="list-group-item board">
                                         <i className="fa fa-flag-checkered pr-1"> Project Board </i>
                                     </li>
                                 </a>
-                                <a href="#">
+                                <a href="# ">
                                     <li className="list-group-item update">
-                                        <i className="fa fa-edit pr-1"> Update Project Info</i>
+                                        <i className="fa fa-edit pr-1"> Update Project</i>
                                     </li>
                                 </a>
                                 <a href="">
@@ -39,4 +39,4 @@ class projectItem extends Component {
     }
 }
 
-export default projectItem;
+export default ProjectItem;
